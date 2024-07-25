fc0fbe451fdc30b082e85bf1fe794e9ab9146f37
As352003-max <amitanshu.22gcebit041@galgotiacollege.edu>
2024-07-25 09:44:16 +0530

    first commit

---
+++ coditions/condition.java
@@ -0,0 +1,12 @@
+public class condition {
+    public static void main(String[] args) {
+        int a=1,b=10;
+        if (a>b){
+            System.out.println("a is largest");
+
+        }
+        else{
+            System.out.println("b is largest");
+        }
+    }
+}
