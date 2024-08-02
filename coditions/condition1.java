95e5b5626c2b15bd7309cf2e1313379f2a86937a
As352003-max <amitanshu.22gcebit041@galgotiacollege.edu>
2024-08-02 10:52:41 +0530

    initial commit

---
+++ coditions/condition1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class condition1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if (a%2==0) {
+            System.out.println("the no is even");
+        } else {
+            System.out.println("the no is odd");
+        }
+        sc.close();
+    }
+}
+
